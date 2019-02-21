a111117e0bcbf7ab8cab289ae6e4953b87f10a07
hajek <1tu56isq>
2019-02-21 22:52:38 +0100

    Configuration ads banner.

--- app/src/main/java/cz/uhk/fimsnake/activity/MainActivity.java
+++ app/src/main/java/cz/uhk/fimsnake/activity/MainActivity.java
@@ -10,20 +10,31 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 import cz.uhk.fimsnake.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AdView mAdView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_main);
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
         ImageButton playButton = findViewById(R.id.playbtn);
 
         playButton.setOnClickListener(new View.OnClickListener() {
