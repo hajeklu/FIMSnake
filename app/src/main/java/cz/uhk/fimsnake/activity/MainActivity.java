cfb964eac844724e322a1c4a42ea1e98062701b3
hajeklu <hajeklu@outlook.com>
2019-06-26 17:46:33 +0200

    Delete useless method. :revolving_hearts:

--- app/src/main/java/cz/uhk/fimsnake/activity/MainActivity.java
+++ app/src/main/java/cz/uhk/fimsnake/activity/MainActivity.java
@@ -102,24 +102,4 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
-
-    public void createNotificationNewRecord() {
-        Intent intent = new Intent(this, TapScore.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel")
-                .setSmallIcon(R.drawable.snake_head)
-                .setContentTitle("My notification")
-                .setContentText("Hello World!")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // Set the intent that will fire when the user taps the notification
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-        notificationManager.notify(1, builder.build());
-
-    }
 }
